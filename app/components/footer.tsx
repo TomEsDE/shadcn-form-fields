3c01ba28b532ab9813beb2762f6ba75370b486ad
Thomas <30468516+TomEsDE@users.noreply.github.com>
2024-10-07 18:16:01 +0200

    footer link optimized

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,31 +5,26 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 export default function Footer() {
   return (
     <div className="bg-cardd py-6">
-      <div className="max-w-6xl mx-auto flex justify-between px-8">
+      <div className="max-w-6xl mx-auto flex justify-between items-center px-8">
         <div className="font-semibold text-primary">© 2024</div>
-        <div className="text-primary/50 flex items-center gap-2">
-          <Image
-            src="/svg_backgrounds_icon.svg"
-            height={0}
-            width={0}
-            className="h-5 w-5"
-            alt="svg-bgs.com"
-          />
-          <a
-            className="hidden md:block"
-            target="_blank"
-            href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/"
-          >
-            Free SVG Backgrounds by SVGBackgrounds.com
-          </a>
-          <a
-            className="block md:hidden"
-            target="_blank"
-            href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/"
-          >
-            SVGBackgrounds.com
-          </a>
-        </div>
+        <a
+          target="_blank"
+          href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/"
+        >
+          <div className="text-primary/50 flex items-center gap-2 text-sm">
+            <div className="block md:hidden">background by</div>
+            <Image
+              src="/svg_backgrounds_icon.svg"
+              height={0}
+              width={0}
+              className="h-5 w-5"
+              alt="svg-bgs.com"
+            />
+            <div className="hidden md:block">
+              Free SVG Backgrounds by SVGBackgrounds.com
+            </div>
+          </div>
+        </a>
         <div className="flex items-center gap-4 md:gap-12 text-sm font-bold underline">
           <Link
             href="https://www.linkedin.com/in/tms-schmidt/"
