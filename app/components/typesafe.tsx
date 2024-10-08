bd2e2ded578644a653b7b6d563a4e776ac046dad
Thomas <30468516+TomEsDE@users.noreply.github.com>
2024-10-08 11:29:55 +0200

    improved text in typesafe-section

--- app/components/typesafe.tsx
+++ app/components/typesafe.tsx
@@ -15,21 +15,22 @@ export default function Typesafe() {
             className="w-auto h-auto max-w-full max-h-full min-h-52 object-contain"
           />
         </div>
-        <div className="md:w-2/5 flex flex-col gap-3 md:gap-8 text-center md:text-left justify-center items-center mmd:text-right order-1 md:order-2">
+        <div className="md:w-2/5 flex flex-col gap-8 text-center md:text-left justify-center items-center mmd:text-right order-1 md:order-2">
           <div className="flex flex-col gap-1">
             <TypescriptIcon className="text-primary h-12 w-12 self-center md:self-start" />
             <div className="font-semibold text-base hidden md:block">
               Integration made easy
             </div>
             <div className="[text-wrap:balance] text-[clamp(1.5rem,15cqw,2rem)]/[1.125] font-bold text-primary">
-              Typesafe and compatible with zod
+              Typesafe with all the power of react-hook-form & zod
             </div>
           </div>
           <div className="font-extralight text-muted-foreground [text-wrap:balance] text-xl">
-            Full typesafe with any custom zod-schema and its corresponding type
+            Fully typesafe with any custom zod-schema and its corresponding type
+            or any type you use with react-hook-form
           </div>
           <div className="font-extralight text-muted-foreground [text-wrap:balance] text-xl">
-            Displays the error messages of your zod-configuration
+            Displays the error messages of your rhf/zod-configuration
           </div>
         </div>
       </div>
